8a0306fc95a5c6ea6f83ac09440bc0344878bcd0
Tim Hagemann <tim@eternalwings.de>
2022-07-01 22:31:59 +0200

    Provide a singleton instance of the password decryptor

--- silkroad-security/src/passcode.rs
+++ silkroad-security/src/passcode.rs
@@ -1,7 +1,22 @@
 use blowfish_compat::{Block, BlockDecrypt, BlowfishCompat, NewBlockCipher};
 use bytes::BytesMut;
+use once_cell::sync::Lazy;
 use std::string::FromUtf8Error;
 
+static INSTANCE: Lazy<PassCodeDecoder> = Lazy::new(|| PassCodeDecoder::default());
+
+/// Decode/Decrypt the password input received by a server from a client when they are logging in. This is a
+/// convenience wrapper around [BlowfishCompat] which automatically uses the correct key. One can use the instance
+/// provided through [get()][PassCodeDecoder::get()] as there's no state to be kept. Use [decode_passcode][Self::decode_passcode()]
+/// to decrypt passcodes.
+///
+/// ```
+/// # use silkroad_security::passcode::PassCodeDecoder;
+/// let input = [113, 42, 1, 64, 127, 104, 60, 94];
+/// let decoder = PassCodeDecoder::get();
+/// let decoded = decoder.decode_passcode(4, &input)?;
+/// assert_eq!(decoded, "1234");
+/// ```
 pub struct PassCodeDecoder {
     blowfish: BlowfishCompat,
 }
@@ -11,6 +26,11 @@ impl PassCodeDecoder {
         let mut data = BytesMut::from(encrypted);
         self.blowfish.decrypt_block(Block::from_mut_slice(&mut data));
         String::from_utf8(data[0..(passcode_length as usize)].to_vec())
+    }
+
+    /// Returns a globally shared instance.
+    pub fn get() -> &Self {
+        &INSTANCE
     }
 }
 
@@ -29,7 +49,7 @@ mod test {
     #[test]
     fn test_simple_decrypt() -> Result<(), FromUtf8Error> {
         let input = [113, 42, 1, 64, 127, 104, 60, 94];
-        let decoder = PassCodeDecoder::default();
+        let decoder = PassCodeDecoder::get();
         let decoded = decoder.decode_passcode(4, &input)?;
         assert_eq!(decoded, "1234");
         Ok(())
