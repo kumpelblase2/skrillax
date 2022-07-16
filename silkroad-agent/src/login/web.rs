45fc0e0991fbb0dab7c77c2667a247b37ae07feb
Tim Hagemann <tim@eternalwings.de>
2022-07-16 21:32:48 +0200

    Use axum instead of salvo for game server rpc

--- silkroad-agent/src/login/web.rs
+++ silkroad-agent/src/login/web.rs
@@ -1,55 +1,45 @@
 use crate::db::user::fetch_server_user;
 use crate::{CapacityController, LoginQueue};
-use salvo::prelude::{FlowCtrl, TcpListener};
-use salvo::{Depot, Handler, Request, Response, Router, Server};
+use axum::response::IntoResponse;
+use axum::routing::{get, post};
+use axum::{Extension, Json, Router, Server};
 use silkroad_rpc::{ReserveRequest, ReserveResponse, ServerStatusReport};
 use sqlx::PgPool;
 use std::net::SocketAddr;
 use std::sync::Arc;
 
-pub(crate) struct HealthStatusHandler {
-    capacity: Arc<CapacityController>,
+#[derive(Clone)]
+struct Capacity(Arc<CapacityController>);
+
+#[derive(Clone)]
+struct Settings(u16, SocketAddr);
+
+async fn handle_capacity(Extension(capacity): Extension<Capacity>) -> impl IntoResponse {
+    let status = ServerStatusReport {
+        healthy: true,
+        population: capacity.0.usage().into(),
+    };
+    Json(status)
 }
 
-#[salvo::async_trait]
-impl Handler for HealthStatusHandler {
-    async fn handle(&self, _: &mut Request, _: &mut Depot, res: &mut Response, _: &mut FlowCtrl) {
-        let status = ServerStatusReport {
-            healthy: true,
-            population: self.capacity.usage().into(),
-        };
-        res.render_json(&status);
-    }
-}
-
-pub(crate) struct SpotReserveHandler {
-    server_id: u16,
-    pool: PgPool,
-    login_queue: LoginQueue,
-    external_address: SocketAddr,
-}
-
-#[salvo::async_trait]
-impl Handler for SpotReserveHandler {
-    async fn handle(&self, req: &mut Request, _: &mut Depot, res: &mut Response, _: &mut FlowCtrl) {
-        let reservation = req.read_from_json::<ReserveRequest>().await.unwrap();
-        let user = fetch_server_user(&self.pool, reservation.user_id, self.server_id)
-            .await
-            .unwrap()
-            .unwrap();
-        match self.login_queue.reserve_spot(user) {
-            Ok((id, duration)) => {
-                res.render_json(&ReserveResponse::Success {
-                    token: id,
-                    ip: self.external_address.ip().to_string(),
-                    port: self.external_address.port(),
-                    alive: duration.as_secs(),
-                });
-            },
-            Err(e) => {
-                res.render_json(&ReserveResponse::Error(format!("No more spots available. {:?}", e)));
-            },
-        }
+async fn handle_spot_request(
+    Extension(settings): Extension<Settings>,
+    Extension(pool): Extension<PgPool>,
+    Extension(login_queue): Extension<LoginQueue>,
+    Json(reservation): Json<ReserveRequest>,
+) -> impl IntoResponse {
+    let user = fetch_server_user(&pool, reservation.user_id, settings.0)
+        .await
+        .unwrap()
+        .unwrap();
+    match login_queue.reserve_spot(user) {
+        Ok((id, duration)) => Json(ReserveResponse::Success {
+            token: id,
+            ip: settings.1.ip().to_string(),
+            port: settings.1.port(),
+            alive: duration.as_secs(),
+        }),
+        Err(e) => Json(ReserveResponse::Error(format!("No more spots available. {:?}", e))),
     }
 }
 
@@ -82,19 +72,20 @@ impl WebServer {
     }
 
     pub async fn run(self) {
-        let status_router = Router::with_path("/status").get(HealthStatusHandler {
-            capacity: self.capacity,
-        });
-        let request_router = Router::with_path("/request").post(SpotReserveHandler {
-            server_id: self.server_id,
-            pool: self.pool,
-            login_queue: self.login_queue,
-            external_address: self.external_address,
-        });
-        let router = Router::new().push(status_router).push(request_router);
+        let router = Router::new()
+            .route("/status", get(handle_capacity))
+            .route("/request", post(handle_spot_request))
+            .layer(Extension(Capacity(self.capacity)))
+            .layer(Extension(Settings(self.server_id, self.external_address)))
+            .layer(Extension(self.login_queue))
+            .layer(Extension(self.pool));
 
-        Server::new(TcpListener::bind(&format!("localhost:{}", self.port)))
-            .serve(router)
-            .await;
+        // TODO: this should be configurable on where it listens on
+        let socket_addr = SocketAddr::from(([127, 0, 0, 1], self.port));
+
+        Server::bind(&socket_addr)
+            .serve(router.into_make_service())
+            .await
+            .unwrap();
     }
 }
