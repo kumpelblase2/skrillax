43853a488112c30f79f2b6aec95b6d76b1d6c460
Tim Hagemann <tim@eternalwings.de>
2022-07-18 21:40:13 +0200

    Add command to drop gold

--- silkroad-agent/src/game/chat.rs
+++ silkroad-agent/src/game/chat.rs
@@ -1,13 +1,16 @@
+use crate::comp::drop::ItemDrop;
 use crate::comp::pos::Position;
 use crate::comp::visibility::Visibility;
-use crate::comp::Client;
+use crate::comp::{Client, GameEntity};
 use crate::event::{ChatEvent, PlayerLevelUp};
 use crate::world::EntityLookup;
 use bevy_app::{App, Plugin};
+use bevy_core::Timer;
 use bevy_ecs::event::EventReader;
 use bevy_ecs::prelude::*;
 use cgmath::num_traits::Pow;
 use cgmath::MetricSpace;
+use id_pool::IdPool;
 use silkroad_protocol::chat::{ChatSource, ChatUpdate};
 use silkroad_protocol::ServerPacket;
 
@@ -22,7 +25,9 @@ impl Plugin for ChatPlugin {
 pub(crate) fn chat_update(
     mut chats: EventReader<ChatEvent>,
     mut levelup: EventWriter<PlayerLevelUp>,
-    lookup: Res<EntityLookup>,
+    mut cmd: Commands,
+    mut id_pool: ResMut<IdPool>,
+    mut lookup: ResMut<EntityLookup>,
     query: Query<(Entity, &Client, &Position, &Visibility)>,
 ) {
     for chat in chats.iter() {
@@ -74,6 +79,26 @@ pub(crate) fn chat_update(
                     "levelup" => {
                         let target: u8 = elements[1].parse().unwrap();
                         levelup.send(PlayerLevelUp(*sender, target));
+                    },
+                    "drop" => {
+                        let amount: u32 = elements[1].parse().unwrap();
+                        let (_, _, pos, _) = query.get(*sender).unwrap();
+                        let id = id_pool.request_id().unwrap();
+                        let drop = cmd
+                            .spawn()
+                            .insert(ItemDrop {
+                                despawn_timer: Timer::from_seconds(10.0, false),
+                                owner: None,
+                                amount,
+                            })
+                            .insert(pos.clone())
+                            .insert(GameEntity {
+                                unique_id: id,
+                                ref_id: 1,
+                            })
+                            .id();
+
+                        lookup.add_entity(id, drop);
                     },
                     _ => {},
                 }
