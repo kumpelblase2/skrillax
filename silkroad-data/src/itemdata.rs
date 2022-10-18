2e5305fd65e1e8b1eeec1407086bf9c97101a4cf
Tim Hagemann <tim@eternalwings.de>
2022-10-18 21:33:31 +0200

    Load required level and biological type from item data

--- silkroad-data/src/itemdata.rs
+++ silkroad-data/src/itemdata.rs
@@ -2,7 +2,7 @@ use crate::type_id::TypeId;
 use crate::{DataEntry, DataMap, FileError, ParseError};
 use num_enum::TryFromPrimitive;
 use pk2::Pk2;
-use std::num::NonZeroU16;
+use std::num::{NonZeroU16, NonZeroU8};
 use std::str::FromStr;
 use std::time::Duration;
 
@@ -38,6 +38,26 @@ pub enum RefItemRarity {
     Legend = 8,
 }
 
+#[derive(TryFromPrimitive, Copy, Clone)]
+#[repr(u8)]
+pub enum BiologicalType {
+    Female = 0,
+    Male = 1,
+    Both = 2,
+    Pet1 = 3,
+    Pet2 = 4,
+    Pet3 = 5,
+}
+
+impl FromStr for BiologicalType {
+    type Err = ParseError;
+
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let value: u8 = s.parse()?;
+        Ok(BiologicalType::try_from(value)?)
+    }
+}
+
 #[derive(Clone)]
 pub struct RefItemData {
     pub ref_id: u32,
@@ -46,8 +66,10 @@ pub struct RefItemData {
     pub despawn_time: Duration,
     pub country: RefItemCountry,
     pub price: u64,
-    pub max_stack_size: usize,
+    pub max_stack_size: u16,
     pub range: Option<NonZeroU16>,
+    pub required_level: Option<NonZeroU8>,
+    pub biological_type: BiologicalType,
     pub params: [isize; 4],
 }
 
@@ -67,6 +89,7 @@ impl FromStr for RefItemData {
     fn from_str(s: &str) -> Result<Self, Self::Err> {
         let elements = s.split('\t').collect::<Vec<&str>>();
         let range: u16 = elements.get(94).ok_or(ParseError::MissingColumn(94))?.parse()?;
+        let required_level: u8 = elements.get(33).ok_or(ParseError::MissingColumn(33))?.parse()?;
         Ok(Self {
             ref_id: elements.get(1).ok_or(ParseError::MissingColumn(1))?.parse()?,
             id: elements.get(2).ok_or(ParseError::MissingColumn(2))?.to_string(),
@@ -86,6 +109,8 @@ impl FromStr for RefItemData {
                 elements.get(124).ok_or(ParseError::MissingColumn(124))?.parse()?,
             ],
             range: NonZeroU16::new(range),
+            required_level: NonZeroU8::new(required_level),
+            biological_type: elements.get(58).ok_or(ParseError::MissingColumn(58))?.parse()?,
             max_stack_size: elements.get(57).ok_or(ParseError::MissingColumn(57))?.parse()?,
         })
     }
