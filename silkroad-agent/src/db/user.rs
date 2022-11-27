60048fb99873370b5b0ce541401e9f9f25c58a06
Tim Hagemann <tim@eternalwings.de>
2022-11-27 22:43:40 +0100

    Fix user not found if no server relation was found for it
    When a user first tries to join a server, their relevant relations to
    the server in the database are not present yet. The LEFT JOIN should've
    supported this behavior, but due to invalid filtering, still resulted in
     an empty response. A correct SQL query would look like this:
    ```
    SELECT users.id, users.username, user_servers.job, user_servers.premium_type, user_servers.premium_end
    FROM users
    LEFT JOIN user_servers on users.id = user_servers.user_id AND server_id = $2
    WHERE id = $1
    ```
    However, as these fields can be null, an `Option` should be used, but
    that gets denied by sqlx. Not using an `Option` results in a runtime
    error, making both ways unusable. Thus, we split this into two queries,
    first fetching the user itself and then the server related data. If no
    data exists, we ensure it will exist for the next login.

--- silkroad-agent/src/db/user.rs
+++ silkroad-agent/src/db/user.rs
@@ -2,7 +2,7 @@ use chrono::{DateTime, Utc};
 use sqlx::{Error, PgPool};
 use std::borrow::Borrow;
 
-#[derive(sqlx::FromRow, Clone)]
+#[derive(sqlx::FromRow, Clone, Debug)]
 pub(crate) struct ServerUser {
     pub id: i32,
     pub username: String,
@@ -19,13 +19,50 @@ impl PartialEq for ServerUser {
 
 impl ServerUser {
     pub async fn fetch<T: Borrow<PgPool>>(id: u32, server: u16, pool: T) -> Result<Option<ServerUser>, Error> {
-        sqlx::query_as!(ServerUser,
-            "SELECT users.id, users.username, user_servers.job, user_servers.premium_type, user_servers.premium_end FROM users LEFT JOIN user_servers on users.id = user_servers.user_id WHERE id = $1 and server_id = $2",
-            id as i32,
-            server as i32
-        )
+        let server_user = match sqlx::query!("SELECT username FROM users WHERE id = $1", id as i32)
+            .fetch_optional(pool.borrow())
+            .await?
+        {
+            Some(user) => {
+                let server_data = sqlx::query!(
+                    "SELECT job, premium_type, premium_end FROM user_servers WHERE user_id = $1 AND server_id = $2",
+                    id as i32,
+                    server as i32
+                )
                 .fetch_optional(pool.borrow())
-                .await
+                .await?;
+                match server_data {
+                    Some(data) => ServerUser {
+                        id: id as i32,
+                        username: user.username,
+                        job: data.job,
+                        premium_type: data.premium_type,
+                        premium_end: data.premium_end,
+                    },
+                    None => {
+                        sqlx::query!(
+                            "INSERT INTO user_servers(user_id, server_id) values($1, $2)",
+                            id as i32,
+                            server as i32
+                        )
+                        .execute(pool.borrow())
+                        .await?;
+
+                        ServerUser {
+                            id: id as i32,
+                            username: user.username,
+                            job: 0,
+                            premium_type: 0,
+                            premium_end: None,
+                        }
+                    },
+                }
+            },
+            None => {
+                return Ok(None);
+            },
+        };
+        Ok(Some(server_user))
     }
 
     pub async fn fetch_job_distribution<T: Borrow<PgPool>>(shard: u16, pool: T) -> (u32, u32) {
