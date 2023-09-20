593f4c8eb6172533e8a939cb68796cd2cdc7d883
Tim Hagemann <tim@eternalwings.de>
2023-09-20 21:09:07 +0200

    Consume SP when levelling up masteries

--- silkroad-agent/src/game/mastery.rs
+++ silkroad-agent/src/game/mastery.rs
@@ -1,9 +1,10 @@
-use bevy_ecs::prelude::*;
-use silkroad_protocol::skill::{LevelUpMasteryError, LevelUpMasteryResponse};
 use crate::comp::net::Client;
 use crate::comp::player::Player;
 use crate::input::PlayerInput;
 use crate::world::WorldData;
+use bevy_ecs::prelude::*;
+use silkroad_protocol::skill::{LevelUpMasteryError, LevelUpMasteryResponse};
+use silkroad_protocol::world::CharacterPointsUpdate;
 
 pub(crate) fn handle_mastery_levelup(mut query: Query<(&Client, &mut Player, &mut PlayerInput)>) {
     let masteries = WorldData::masteries();
@@ -15,18 +16,37 @@ pub(crate) fn handle_mastery_levelup(mut query: Query<(&Client, &mut Player, &mu
                 continue;
             };
 
-            let current_level = player.character.masteries.iter().find(|(mastery, _)| mastery.ref_id == mastery_levelup.mastery as u16).map(|(_, level)| level).copied().unwrap_or(0);
-            let next_level = current_level.checked_add(1).unwrap_or(u8::MAX);
-            let required_sp = levels.get_mastery_sp_for_level(next_level).expect("Should contain info about required mastery sp.");
+            let current_level = player
+                .character
+                .masteries
+                .iter()
+                .find(|(mastery, _)| mastery.ref_id == mastery_levelup.mastery as u16)
+                .map(|(_, level)| level)
+                .copied()
+                .unwrap_or(0);
+
+            let required_sp = levels.get_mastery_sp_for_level(current_level).unwrap_or(0);
 
             if player.character.sp < required_sp {
                 client.send(LevelUpMasteryResponse::Error(LevelUpMasteryError::InsufficientSP));
                 continue;
             }
 
-            player.character.sp -= required_sp;
+            if required_sp > 0 {
+                player.character.sp -= required_sp;
+                client.send(CharacterPointsUpdate::SP {
+                    amount: player.character.sp,
+                    display: false,
+                });
+            }
 
-            if let Some(position) = player.character.masteries.iter().position(|(mastery, _)| mastery.ref_id == mastery_levelup.mastery as u16) {
+            let next_level = current_level.checked_add(1).unwrap_or(u8::MAX);
+            if let Some(position) = player
+                .character
+                .masteries
+                .iter()
+                .position(|(mastery, _)| mastery.ref_id == mastery_levelup.mastery as u16)
+            {
                 player.character.masteries[position] = (mastery, next_level);
             } else {
                 player.character.masteries.push((mastery, next_level));
