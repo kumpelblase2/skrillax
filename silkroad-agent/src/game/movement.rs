ce62ff01a55afa3baa4d2c36c9648c3c4dd4d345
Tim Hagemann <tim@eternalwings.de>
2023-11-06 13:32:57 +0100

    Don't stroll if another thing is occupying the mind

--- silkroad-agent/src/game/movement.rs
+++ silkroad-agent/src/game/movement.rs
@@ -2,18 +2,25 @@ use crate::agent::states::{Idle, MovementGoal, Moving, StateTransitionQueue};
 use crate::comp::monster::RandomStroll;
 use crate::comp::pos::Position;
 use crate::ext::Navmesh;
+use crate::game::mind::Mind;
 use bevy_ecs::prelude::*;
 use bevy_time::Time;
 use rand::random;
 use silkroad_game_base::{GlobalLocation, Vector2Ext};
 
 pub(crate) fn movement_monster(
-    mut query: Query<(&mut RandomStroll, &mut StateTransitionQueue, &Position), With<Idle>>,
+    mut query: Query<(&mut RandomStroll, &mut StateTransitionQueue, &Position, Option<&Mind>), With<Idle>>,
     delta: Res<Time>,
     navmesh: Res<Navmesh>,
 ) {
     let delta = delta.delta();
-    for (mut stroll, mut transition, pos) in query.iter_mut() {
+    for (mut stroll, mut transition, pos, mind) in query.iter_mut() {
+        if let Some(mind) = mind {
+            if mind.has_goal() {
+                continue;
+            }
+        }
+
         if stroll.check_timer.finished() && random::<f32>() <= 0.1 {
             let new_location = GlobalLocation(stroll.origin.0.random_in_radius(stroll.radius));
             let new_y = navmesh.height_for(new_location).unwrap_or(pos.position().0.y);
